32726da5945f171e287d9ff98a0729ae1a4444e3
Cyanyoel Yoon <ycy1766@gmail.com>
2019-10-21 17:38:59 +0900

    update ceph/infra node tf

--- scripts/04_deploy_ceph.sh
+++ scripts/04_deploy_ceph.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-TYPE="default"
+TYPE="deploy_ceph"
 
 TERRAFORM_BIN="${PWD}/../bin/terraform"
 
@@ -9,15 +9,14 @@ STATE_DIR="${PWD}/../state"
 PLUGINS_DIR="${PWD}/../plugins"
 
 ${TERRAFORM_BIN} init -plugin-dir=${PLUGINS_DIR} -input=false ${WORK_DIR}
-
 if [ $? -ne 0 ]; then
     exit 1;
 fi
 
-
 ${TERRAFORM_BIN} apply -auto-approve \
  -var-file=${WORK_DIR}/../default.tfvars \
  -var-file=${WORK_DIR}/../networks.tfvars \
+ -var-file=${WORK_DIR}/../${TYPE}.tfvars \
  -state=${STATE_DIR}/${TYPE}/terraform.tfstate \
  -input=false ${WORK_DIR}
 
